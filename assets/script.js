cd1b70baf0c0b51f0ca4c9509925681d33c364e2
L.J Solis <l.jsolis@L.lan>
2023-06-03 12:58:19 -0500

    added JavaScript

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,13 @@
 var topics = ['HTML', 'CSS', 'Git', 'Javascript'];
 
+function listTopics() {
 for (var x = 0; x < topics.length; x++) {
     console.log(topics[x]);
 }
+}
 
 
-/*
+function selectTopic() {
 if (topic === 'HTML') {
     console.log("Let's study HTML!");
 } else if (topic === 'CSS') {
@@ -17,9 +19,4 @@ if (topic === 'HTML') {
 } else {
     console.log('Please try again!');
 }
-
-var shapes = ["triangle", "square", "pentagon", "circle"];
-for(var x = 0; x < shapes.length; x++) {
-    console.log(shapes[x]);
 }
-*/
